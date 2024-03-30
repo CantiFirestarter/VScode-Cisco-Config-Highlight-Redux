abda9194b93bb728ad66c55e2b71f5f0aebffe6f
yuyosy <y-ysss@outlook.com>
2024-03-30 23:14:47 +0900

    apply formatter

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,8 @@
 import * as vscode from 'vscode';
-import { registerUpdateInfo } from './registerUpdateInfo';
 import { registerOutlineSymbolProvider } from './registerOutlineSymbol';
+import { registerUpdateInfo } from './registerUpdateInfo';
 
 export function activate(context: vscode.ExtensionContext): void {
-    registerUpdateInfo(context);
-    registerOutlineSymbolProvider(context);
+  registerUpdateInfo(context);
+  registerOutlineSymbolProvider(context);
 }
--- src/registerUpdateInfo.ts
+++ src/registerUpdateInfo.ts
@@ -1,64 +1,60 @@
-import * as vscode from 'vscode';
 import * as semver from 'semver';
+import * as vscode from 'vscode';
 import { notificationConditions, NotificationInfo } from './notificationConditions';
 
-
 export function registerUpdateInfo(context: vscode.ExtensionContext) {
-    // console.log('Hello');
-    const versionKey = `previous_version`;
-    context.globalState.setKeysForSync([versionKey]);
+  // console.log('Hello');
+  const versionKey = `previous_version`;
+  context.globalState.setKeysForSync([versionKey]);
 
-    // https://code.visualstudio.com/api/extension-capabilities/common-capabilities
-    const previousVersion: string | undefined = context.globalState.get(versionKey);
-    const currentVersion = context.extension.packageJSON.version;
-    context.globalState.update(versionKey, currentVersion);
-    // context.globalState.update(versionKey, '0.3.6');   // -----------------------------DEBUG
-    // console.log(context.globalState.keys());
-    console.log(previousVersion, currentVersion);
+  // https://code.visualstudio.com/api/extension-capabilities/common-capabilities
+  const previousVersion: string | undefined = context.globalState.get(versionKey);
+  const currentVersion = context.extension.packageJSON.version;
+  context.globalState.update(versionKey, currentVersion);
+  // context.globalState.update(versionKey, '0.3.6');   // -----------------------------DEBUG
+  // console.log(context.globalState.keys());
+  console.log(previousVersion, currentVersion);
 
-    if (previousVersion && isIgnore(previousVersion, currentVersion)) {
-        return;
+  if (previousVersion && isIgnore(previousVersion, currentVersion)) {
+    return;
+  }
+  notificationConditions.forEach(info => {
+    if (!previousVersion || semver.satisfies(previousVersion, info.version_info)) {
+      const prev = previousVersion ? previousVersion : 'undefined';
+      let message = info.messege.split('${previousVersion}').join(prev);
+      message = message.split('${currentVersion}').join(currentVersion);
+      const dialog = getDialog(info, message);
+      dialog.then(() => {
+        info.action(prev, currentVersion);
+      });
     }
-    notificationConditions.forEach(info => {
-        if (!previousVersion || semver.satisfies(previousVersion, info.version_info)) {
-            let prev = previousVersion ? previousVersion : 'undefined'
-            let message = info.messege.split('${previousVersion}').join(prev);
-            message = message.split('${currentVersion}').join(currentVersion);
-            const dialog = getDialog(info, message);
-            dialog.then(() => {
-                info.action(prev, currentVersion);
-            });
-        }
-    });
+  });
 }
 
-function getDialog(info: NotificationInfo, message: string): Thenable<string | undefined> {
-    if (info.type === 'info') {
-        return vscode.window.showInformationMessage(
-            message,
-            info.button_label,
-        );
-    } else if (info.type === 'warn') {
-        return vscode.window.showWarningMessage(
-            message,
-            info.button_label,
-        );
-    } else {
-        return vscode.window.showInformationMessage(
-            message,
-            info.button_label,
-        );
-    }
+function getDialog(
+  info: NotificationInfo,
+  message: string,
+): Thenable<string | undefined> {
+  if (info.type === 'info') {
+    return vscode.window.showInformationMessage(message, info.button_label);
+  } else if (info.type === 'warn') {
+    return vscode.window.showWarningMessage(message, info.button_label);
+  } else {
+    return vscode.window.showInformationMessage(message, info.button_label);
+  }
 }
 
 function isIgnore(previousVersion: string, currentVersion: string): boolean {
-    const isLessThan = semver.lt(previousVersion, currentVersion);
-    if (isLessThan) {
-        const differs: semver.ReleaseType | null = semver.diff(previousVersion, currentVersion);
+  const isLessThan = semver.lt(previousVersion, currentVersion);
+  if (isLessThan) {
+    const differs: semver.ReleaseType | null = semver.diff(
+      previousVersion,
+      currentVersion,
+    );
 
-        if (!differs || differs === "patch") {
-            return true;
-        }
+    if (!differs || differs === 'patch') {
+      return true;
     }
-    return !isLessThan
+  }
+  return !isLessThan;
 }
