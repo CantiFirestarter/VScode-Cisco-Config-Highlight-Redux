cb0fe8875637ce636dcd9e7b1122687905a35f17
Canti Firestarter <lordcantix@gmail.com>
2026-01-07 11:20:04 -0600

    Notable Changes

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 import * as vscode from 'vscode';
 import { registerOutlineSymbolProvider } from './registerOutlineSymbol';
+import { registerTokenColorCustomization } from './registerTokenColors';
 import { registerUpdateInfo } from './registerUpdateInfo';
 
 export const outputChannel = vscode.window.createOutputChannel('Cisco Config Highlight');
@@ -8,7 +9,10 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
   try {
     await registerUpdateInfo(context);
   } catch (err) {
-    outputChannel.append(String(err));
+    outputChannel.appendLine(String(err));
   }
   registerOutlineSymbolProvider(context);
+
+  // Register token color customization (Option 2)
+  registerTokenColorCustomization(context);
 }
